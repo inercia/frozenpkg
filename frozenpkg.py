eb13b2cfffd920ff34e416ecd794017d24c1fa66
Alvaro Saurin <saurin@tid.es>
2010-03-25 00:04:30 +0100

    Updates

--- frozenpkg.py
+++ frozenpkg.py
@@ -25,10 +25,17 @@ class FrozenRPM:
         logging.getLogger(self.name).info(msg)
 
     def install(self):
-        pkg_name    = self.options['pkg-name']
-        pkg_version = self.options['pkg-version']
+        install_prefix = self.options['install-prefix']
+        pkg_name       = self.options['pkg-name']
+        pkg_version    = self.options['pkg-version']
+        pkg_vendor     = self.options['pkg-vendor']
+        pkg_packager   = self.options['pkg-packager']
+        pkg_url        = self.options['pkg-url']
+        pkg_license    = self.options['pkg-license']
+        pkg_group      = self.options['pkg-group']
 
-        buildroot   = os.path.normpath(tempfile.mkdtemp() + "/" + self.options['install-prefix'])
+
+        buildroot   = os.path.normpath(tempfile.mkdtemp() + "/" + install_prefix)
 
         # 1. copy the python bins
 
@@ -36,10 +43,9 @@ class FrozenRPM:
         bins_ddir   = os.path.normpath(buildroot + "/bin")
 
         self._log('Copying binary python')
-        try:
-            os.makedirs(bins_ddir)
-        except Exception,e :
-            pass
+        try: os.makedirs(bins_ddir)
+        except Exception: pass
+
         shutil.copy(bins_sdir + "/python", bins_ddir)
 
         # 2. copy the libs
@@ -51,27 +57,26 @@ class FrozenRPM:
         eggs_ddir = os.path.normpath(buildroot + "/lib/python2.6/site-packages/")
 
         for egg_name in self.options['eggs']:
-            for egg_path in glob.glob(eggs_sdir + "/" + egg_name + "*.egg"):
-                self._log('Copying egg %s' % os.path.basename(egg_path))
-                egg_dest = os.path.normpath(eggs_ddir + "/" + os.path.basename(egg_path))
-                shutil.copytree(egg_path, egg_dest)
+            for egg_path in os.listdir(eggs_sdir):
+                if fnmatch.fnmatch(egg_path, egg_name + "*.egg"):
+                    self._log('Copying egg %s' % os.path.basename(egg_path))
+                    egg_dest = os.path.normpath(eggs_ddir + "/" + os.path.basename(egg_path))
+                    shutil.copytree(egg_path, egg_dest)
 
 
         rpmspec = """
-            %define installdir
-            %define buildoutcfg   buildout.cfg
             %define buildroot     @BUILDROOT@
 
-            Name:                 @PKGNAME@
-            Version:              1.0
+            Name:                 @PKG_NAME@
+            Version:              @PKG_VERSION@
             Release:              0
-            Summary:              urldownloader
-            URL:                  http://www.tid.es
-            License:              (c) 2010 Telefonica I+D
-            Vendor:               Telefonica I+D
-            Packager:             Telefonica I+D
-            Group:                Applications/Networking
-            Buildroot:            @BUILDROOT@
+            Summary:              @PKG_NAME@
+            URL:                  @PKG_URL@
+            License:              @PKG_LICENSE@
+            Vendor:               @PKG_VENDOR@
+            Packager:             @PKG_PACKAGER@
+            Group:                @PKG_GROUP@
+            Buildroot:            @BUILD_ROOT@
 
             %description
 
@@ -89,9 +94,15 @@ class FrozenRPM:
 
             %clean
             """
-        rpmspec.replace("@PKGNAME@", pkg_name)
-        rpmspec.replace("@INSTALLPREFIX@", install-prefix)
-        rpmspec.replace("@BUILDROOT@", buildroot)
+        rpmspec.replace("@PKG_NAME@",        pkg_name)
+        rpmspec.replace("@PKG_VENDOR@",      pkg_vendor)
+        rpmspec.replace("@PKG_VERSION@",     pkg_version)
+        rpmspec.replace("@PKG_PACKAGER@",    pkg_packager)
+        rpmspec.replace("@PKG_URL@",         pkg_url)
+        rpmspec.replace("@PKG_LICENSE@",     pkg_license)
+        rpmspec.replace("@PKG_GROUP@",       pkg_group)
+        rpmspec.replace("@INSTALL_PREFIX@",  install_prefix)
+        rpmspec.replace("@BUIL_DROOT@",      buildroot)
 
         with tempfile.mktemp() as f:
             f.write(rpmspec)
