a5fbc96d4f62af4accdbbf3be248835f708c2454
Alvaro Saurin <saurin@Arnoia>
2010-04-07 13:23:12 +0200

    New release

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ setup(
     name             = "as.recipe.frozenpkg",
     description      = "ZC Buildout recipe for freezing buildouts in RPM's, tar.gz's, etc",
     long_description = long_description,
-    version          = '0.1.10',
+    version          = '0.1.11',
 
     # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
     classifiers = [
