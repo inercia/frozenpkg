d4b865576e15b30df1268e5fce0327df3dbb4277
Alvaro Saurin <saurin@Arnoia>
2010-04-07 12:53:12 +0200

    Minor fixes

--- as/recipe/frozenpkg/__init__.py
+++ as/recipe/frozenpkg/__init__.py
@@ -249,7 +249,10 @@ class FrozenRPM(object):
         # replace "sys.path[0:0] = [" by the same string PLUS the new
         # library path...
         base_string     = "sys.path[0:0] = ["
-        new_base_string = base_string + "\n  '" + rel_lib_ddir + "',\n  '" + rel_site_lib_ddir + "',"
+        new_base_string = base_string + "\n" + \
+                          "  '" + rel_lib_ddir                      + "',\n" + \
+                          "  '" + rel_lib_ddir + "/lib-dynload"     + "',\n" + \
+                          "  '" + rel_site_lib_ddir                 + "',"
         
         replacements = replacements + [
                         (base_string, new_base_string)
