c67a01fbcf02eca55750c33dd73bcc3421217401
Alvaro Saurin <saurin@Arnoia>
2010-05-21 09:59:42 +0200

    Refactored

--- setup.py
+++ setup.py
@@ -109,10 +109,10 @@ Known Bugs
 
 
 setup(
-    name             = "as.recipe.frozenpkg",
-    description      = "ZC Buildout recipe for freezing buildouts in RPM's, tar.gz's, etc",
+    name = "as.recipe.frozenpkg",
+    description = "ZC Buildout recipe for freezing buildouts in RPM's, tar.gz's, etc",
     long_description = long_description,
-    version          = '0.2.1',
+    version = '0.2.1',
 
     # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
     classifiers = [
@@ -122,27 +122,27 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         ],
-    keywords     = 'buildout recipe',
-    author       = 'Alvaro Saurin',
+    keywords = 'buildout recipe',
+    author = 'Alvaro Saurin',
     author_email = 'name dot surname at gmail.com',
 
-    packages             = find_packages(exclude=['ez_setup']),
-    namespace_packages   = ['as', 'as.recipe'],
+    packages = find_packages(exclude = ['ez_setup']),
+    namespace_packages = ['as', 'as.recipe'],
     include_package_data = True,
-    install_requires     = [
+    install_requires = [
         'setuptools',
         'zc.buildout'
         # -*- Extra requirements: -*-
     ],
 
-    license  = 'GPL',
+    license = 'GPL',
     zip_safe = False,
     entry_points = {
         'zc.buildout': [
             'default = as.recipe.frozenpkg:FrozenRPM',
-            
-            'rpm     = as.recipe.frozenpkg:FrozenRPM',
-            'tgz     = as.recipe.frozenpkg:FrozenTgz'
+
+            'rpm     = as.recipe.frozenpkg.frozenrpm:FrozenRPM',
+            'tgz     = as.recipe.frozenpkg.frozentgz:FrozenTgz'
         ]
     },
 )
